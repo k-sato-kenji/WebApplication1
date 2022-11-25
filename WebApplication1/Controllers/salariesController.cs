b3d96d83f50acb1e0c6dea7db3dd6b6edeeb8bd6
賢治 佐藤 <sato-kenji@open-source.co.jp>
2022-11-25 14:51:41 +0900

    first commit

---
+++ WebApplication1/Controllers/salariesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class salariesController : Controller
+    {
+        private readonly WebApplication1Context _context;
+
+        public salariesController(WebApplication1Context context)
+        {
+            _context = context;
+        }
+
+        // GET: salaries
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.salary.ToListAsync());
+        }
+
+        // GET: salaries/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salary = await _context.salary
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (salary == null)
+            {
+                return NotFound();
+            }
+
+            return View(salary);
+        }
+
+        // GET: salaries/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: salaries/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,beneficiary,paymenttype,paymentdate,paymentamount,travelexpence,healthinsurancepremium,welfarepension,employmentinsurancepremium,incometax,residenttax,totalpaymentamount,overtimeallowance,midnightallowance,holidayallowance,remarks")] salary salary)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(salary);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(salary);
+        }
+
+        // GET: salaries/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salary = await _context.salary.FindAsync(id);
+            if (salary == null)
+            {
+                return NotFound();
+            }
+            return View(salary);
+        }
+
+        // POST: salaries/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,beneficiary,paymenttype,paymentdate,paymentamount,travelexpence,healthinsurancepremium,welfarepension,employmentinsurancepremium,incometax,residenttax,totalpaymentamount,overtimeallowance,midnightallowance,holidayallowance,remarks")] salary salary)
+        {
+            if (id != salary.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(salary);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!salaryExists(salary.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(salary);
+        }
+
+        // GET: salaries/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salary = await _context.salary
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (salary == null)
+            {
+                return NotFound();
+            }
+
+            return View(salary);
+        }
+
+        // POST: salaries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var salary = await _context.salary.FindAsync(id);
+            _context.salary.Remove(salary);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool salaryExists(int id)
+        {
+            return _context.salary.Any(e => e.id == id);
+        }
+    }
+}
---
+++ WebApplication1/Models/salary.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class salary
+    {
+
+        // ID
+        public int id { get; set; }
+        // 受給者
+        [Display(Name = "受給者")]
+        [Required(ErrorMessage = "受給者は必須入力です")]
+        public string beneficiary { get; set; }
+        // タイプ（給与、賞与）
+        [Display(Name = "給与/賞与")]
+        [Required(ErrorMessage = "給与/賞与は必須入力です")]
+        public string paymenttype { get; set; }
+
+        // 支給日
+        [Display(Name = "支給日")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")] //フォーマットの指定
+        [DataType(DataType.Date)]                           //DataTypeの指定
+        [Required(ErrorMessage = "支給日は必須入力です")]
+        public DateTime paymentdate { get; set; }
+
+
+        // 支給額
+        [Display(Name = "支給額")]
+        [Required(ErrorMessage = "支給額は必須入力です")]
+        public decimal paymentamount { get; set; }
+        // 交通費
+        [Display(Name = "交通費")]
+        public decimal travelexpence { get; set; }
+        // 健康保険料
+        [Display(Name = "健康保険料")]
+        public decimal healthinsurancepremium { get; set; }
+        // 厚生年金料
+        [Display(Name = "厚生年金料")]
+        public decimal welfarepension { get; set; }
+        // 雇用保険料
+        [Display(Name = "雇用保険料")]
+        public decimal employmentinsurancepremium { get; set; }
+        // 所得税
+        [Display(Name = "所得税")]
+        public decimal incometax { get; set; }
+        // 住民税
+        [Display(Name = "住民税")]
+        public decimal residenttax { get; set; }
+        // 総支給
+        [Display(Name = "総支給額")]
+        public decimal totalpaymentamount { get; set; }
+        // 時間外手当
+        [Display(Name = "時間外手当")]
+        public decimal overtimeallowance { get; set; }
+        // 深夜手当
+        [Display(Name = "深夜手当")]
+        public decimal midnightallowance { get; set; }
+        // 休日手当
+        [Display(Name = "休日手当")]
+        public decimal holidayallowance { get; set; }
+        // 備考
+        [Display(Name = "備考")]
+        public string remarks { get; set; }
+
+    }
+}
